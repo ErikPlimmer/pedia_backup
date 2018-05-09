801250b72a20bc97df8a2194387fc47a41e7ee7b
Erik Plimmer <Elp013@Gmail.com>
2018-05-09 19:03:09 -0400

    run full stack

--- APIroutes/API.js
+++ APIroutes/API.js
@@ -5,7 +5,7 @@ function proxyEdamam(request, response) {
   (requestProxy({
     url: `https://api.edamam.com/search`,
     headers: {
-      app_id: 'ID',
-      app_key: 'KEY'}
+      app_id: 'f135b876',
+      app_key: 'acdb60a92956d657fa8aacf40beb71c6'}
   }))(request, response);
 }
